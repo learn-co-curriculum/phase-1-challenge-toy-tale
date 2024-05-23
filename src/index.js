a50eb4b8d6a2473b3977f82a98320032064af5fe
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-05-22 20:02:33 -0400

    rework problem for practice

--- src/index.js
+++ src/index.js
@@ -8,86 +8,97 @@ document.addEventListener("DOMContentLoaded", () => {
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
-      } else {
+    } else {
       toyFormContainer.style.display = "none";
-      }
-    });
+    }
+  });
 });
-/////////////////////////////////////////////////////////////////////////////
-const toyURL = "http://localhost:3000/toys"
-const toyCollection = document.getElementById("toy-collection") 
 
-function renderOneToy(toy) {
-  //build card
-  let card = document.createElement('div')
-  card.classList.add("card")
-  card.innerHTML = `
-    <h2>${toy.name}</h2>
-    <img src="${toy.image}" class="toy-avatar" />
-    <p>${toy.likes} likes</p>
-    <button class="like-btn" id="${toy.id}">Like ❤️</button>`
-    toyCollection.appendChild(card)
-  const button = document.getElementById(`${toy.id}`)
+const url = "http://localhost:3000/toys"
+const collection = document.getElementById("toy-collection") 
+// toy data = [ {id, name, image, likes] } array of objects
+
+fetch(url)
+  .then((response) => response.json())
+  .then((toyData)  => toyData.forEach(toy => renderOneCard(toy)))
+
+
+function renderOneCard(toy) {
+
+  const div = document.createElement("div")
+  const h2 = document.createElement("h2")
+  const img = document.createElement("img")
+  const p = document.createElement("p")
+  const button = document.createElement("button")
+
+  div.className = "card"
+  h2.innerText = toy.name
+  img.src = toy.image
+  img.className = "toy-avatar"
+  p.innerText = `${toy.likes} likes` 
+  button.className = "like-btn"
+  button.id = `${toy.id}`
+  button.innerText = "like ❤️"
+
+  collection.appendChild(div)
+  div.appendChild(h2)
+  div.appendChild(img)
+  div.appendChild(p)
+  div.appendChild(button)
+
   button.addEventListener("click", () => {
     toy.likes++
-    card.querySelector("p").textContent = `${toy.likes} likes` 
+    p.innerText = `${toy.likes} likes`
 
     fetch(`http://localhost:3000/toys/${toy.id}`, {
       method: "PATCH",
-      header: 
-      {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-      body: JSON.stringify({
-        "likes" : `${toy.likes}`
-      })
-    })
+      headers:
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      likes: `${toy.likes}`
+    }),
+  })
   })
 }
 
+const form = document.querySelector(".container") 
 
-function getAllToys() {
-  fetch(toyURL)
-  .then((response) => response.json())
-  .then((toyData) => toyData.forEach(toy => renderOneToy(toy)))
-}
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
 
-const form = document.querySelector("form.add-toy-form")
+  const toyValue = document.querySelectorAll(".input-text")[0].value
+  const imageValue = document.querySelectorAll(".input-text")[1].value 
 
-form.addEventListener("submit", handlesSubmit)
-
-function handlesSubmit(event) {
-  event.preventDefault();
-  const toyValue = document.getElementById("nameInput").value
-  const imageValue = document.getElementById("picInput").value
-
-  let newToyObject = {
+let newToy = {
     name: toyValue,
     image: imageValue,
-    likes: 0,
-  };
-  addNewToy(newToyObject)
-}
-
-function addNewToy(newToyObject) {
-
-  const configArray = {
-    method: "POST",
-    headers: 
-    {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify(newToyObject)
+    likes: 0
   }
-  fetch(toyURL, configArray)
-    .then((response)=> response.json())
-    .then((toyData) => renderOneToy(newToyObject))
-}
 
-function initialize() {
-  getAllToys()
-}
+  fetch(url, {
+    method: "POST",
+    headers:
+  {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(newToy),
+})
+  .then((response) => response.json())
+  .then((toy)  => renderOneCard(toy))
+})
 
-initialize()
+// grab like button
+// 
+const allButtons = document.querySelectorAll(".like-btn")
+
+// // allButtons.forEach((button) => {
+// //   button.addEventListener("click", () => {
+// //     console.log(click)
+// //     // toy.likes++
+// //     // p.textContent = `${toy.likes} likes`
+//   })
+// })
