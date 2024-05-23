cca7b5677694333c99fe159918c37600c8e02b5b
Liam Simmons <liam.simmons@powerhrg.com>
2024-05-23 14:39:48 -0400

    add delete button to toy cards

--- src/index.js
+++ src/index.js
@@ -25,17 +25,23 @@ const toyCardServerToDom = () => {
   .then(resp => {
     toysArray = resp
     return resp
-})
+  })
   .then(array => {
     array.forEach(toy => {
-      const cardContents = 
-      `<H2>${toy.name}</H2>
-      <img src="${toy.image}" class="toy-avatar">
-      <p class="${toy.name}">${toy.likes} Likes</p>
-      <button class="like-btn" id="${toy.id}">Like ❤️</button>`
+      const cardContents =
+        `<H2>${toy.name}</H2>
+        <img src="${toy.image}" class="toy-avatar">
+        <p class="${toy.name}">${toy.likes} Likes</p>
+        <button class="like-btn" id="${toy.id}">Like ❤️</button>
+        <button class="delete-btn" id="${toy.id}-for-delete">Delete</button>`
+
       const createDiv = document.createElement('div')
       createDiv.classList.add('card')
+
       toyCollection.appendChild(createDiv).innerHTML = cardContents
+
+      addDeleteButtonListener(toy)
+
       addLikeButtonListener(toy)
     })
   })
@@ -49,43 +55,54 @@ addToyForm.addEventListener('submit', (event) => {
     "name": `${newToyName.value}`,
     "image": `${newToyImage.value}`,
     "likes": 0
-}
-fetchNewToy(requestBody)
+  }
+  fetchNewToy(requestBody)
 })
 
-const fetchNewToy = (requestBody) => {
- const options = {
-    method: 'POST',
-    headers:
-    {
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    },
-    body: JSON.stringify(requestBody)
- }
-fetch('http://localhost:3000/toys', options)
-.then(resp => resp.json())
-.then(resp => {
-  const cardContents = 
-      `<H2>${resp.name}</H2>
-      <img src="${resp.image}" class="toy-avatar">
-      <p class="${resp.name}">${resp.likes} Likes</p>
-      <button class="like-btn" id="${resp.id}">Like ❤️</button>`
+  const fetchNewToy = (requestBody) => {
+    const options = {
+      method: 'POST',
+      headers:
+      {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(requestBody)
+    }
+  fetch('http://localhost:3000/toys', options)
+    .then(resp => resp.json())
+    .then(resp => {
+      const cardContents =
+        `<H2>${resp.name}</H2>
+        <img src="${resp.image}" class="toy-avatar">
+        <p class="${resp.name}">${resp.likes} Likes</p>
+        <button class="like-btn" id="${resp.id}">Like ❤️</button>
+        <button class="delete-btn" id="${resp.id}-for-delete">Delete</button>`
+
       const createDiv = document.createElement('div')
       createDiv.classList.add('card')
       toyCollection.appendChild(createDiv).innerHTML = cardContents
+
+      addDeleteButtonListener(resp)
       addLikeButtonListener(resp)
-})
+    })
 }
 
 const addLikeButtonListener = (toy) => {
   const likeButton = document.getElementById(`${toy.id}`)
+
   likeButton.addEventListener('click', (event) => {
     event.preventDefault()
     updateLikes(toy)
   })
 }
 
+const addDeleteButtonListener = (toy) => {
+  document.getElementById(`${toy.id}-for-delete`).addEventListener('click', (event) => {
+    event.preventDefault()
+    deleteCard(toy.id)
+  })
+}
 
 const updateLikes = (toy) => {
   const likesDisplay = document.getElementsByClassName(`${toy.name}`)[0].textContent.split(" ")[0]
@@ -100,23 +117,25 @@ const updateLikes = (toy) => {
       "likes": parseInt(likesDisplay) + 1
     })
   }
+
   fetch(`http://localhost:3000/toys/${toy.id}`, options)
-.then(resp => {
-  return resp.json()
-})
-.then(resp => {
-  document.getElementsByClassName(`${toy.name}`)[0].textContent = `${resp.likes} Likes`
-})
+    .then(resp => {
+      return resp.json()
+    })
+    .then(resp => {
+      document.getElementsByClassName(`${toy.name}`)[0].textContent = `${resp.likes} Likes`
+    })
 }
 
-
-
-
-
-
-
-
-//Add 'click' event listener to like button
-//prevent page reload
-//send patch request to increment like property on server
-//then update the dom
+const deleteCard = (toy) => {
+  const options = {
+    method: 'DELETE',
+    headers:
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    }
+  }
+  fetch(`http://localhost:3000/toys/${toy}`, options)
+  .then(_resp => document.getElementById(`${toy}`).parentElement.remove())
+}
