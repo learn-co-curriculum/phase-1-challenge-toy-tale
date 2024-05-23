bb98052d5754d63d66c4c5ab35dd414892508c60
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-05-22 23:56:15 -0400

    Reworked and added delete button

--- src/index.js
+++ src/index.js
@@ -16,89 +16,100 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const url = "http://localhost:3000/toys"
 const collection = document.getElementById("toy-collection") 
-// toy data = [ {id, name, image, likes] } array of objects
+const toyCard = document.querySelector(".card")
+// toyData = [ {id, name, image, likes} ]
 
 fetch(url)
-  .then((response) => response.json())
-  .then((toyData)  => toyData.forEach(toy => renderOneCard(toy)))
+.then((respone) => respone.json())
+.then((toyData) => toyData.forEach(toy => renderOneToy(toy)))
 
-
-function renderOneCard(toy) {
+function renderOneToy(toy) {
 
   const div = document.createElement("div")
   const h2 = document.createElement("h2")
   const img = document.createElement("img")
-  const p = document.createElement("p")
+  const p = document.createElement("div")
   const button = document.createElement("button")
+  const delButton = document.createElement("button")
 
-  div.className = "card"
-  h2.innerText = toy.name
+  div.classList.add("card")
+  h2.textContent = `${toy.name}`
   img.src = toy.image
-  img.className = "toy-avatar"
-  p.innerText = `${toy.likes} likes` 
-  button.className = "like-btn"
+  img.classList.add("toy-avatar")
+  p.textContent = `${toy.likes} likes`
+  button.classList.add("like-btn")
   button.id = `${toy.id}`
-  button.innerText = "like ❤️"
+  button.textContent ="Like ❤️"
+  delButton.textContent ="Give to Sid"
+  delButton.id = "delete-btn"
 
   collection.appendChild(div)
   div.appendChild(h2)
   div.appendChild(img)
   div.appendChild(p)
   div.appendChild(button)
+  div.appendChild(delButton)
 
   button.addEventListener("click", () => {
     toy.likes++
-    p.innerText = `${toy.likes} likes`
+    p.textContent = `${toy.likes} likes`
 
     fetch(`http://localhost:3000/toys/${toy.id}`, {
       method: "PATCH",
-      headers:
-    {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({
-      likes: `${toy.likes}`
-    }),
+      headers: 
+      {
+        "Content-Type" : `application/json`,
+        Accept: "application/json"
+      },
+      body: JSON.stringify( {
+        "likes": `${toy.likes}`
+      }),
+    })
+    .then((response) => response.json())
+    .then((data) => console.log (data))
   })
+
+  delButton.addEventListener("click", () => {
+    const cardDelete = delButton.closest('.card')
+    cardDelete.remove()
+    handleDelete(toy.id)
   })
+
 }
 
-const form = document.querySelector(".container") 
+const form = document.querySelector(".add-toy-form") 
 
 form.addEventListener("submit", (event) => {
   event.preventDefault()
+  const toyInput = (document.querySelectorAll(".input-text")[0].value)
+  const imageInput = (document.querySelectorAll(".input-text")[1].value)
 
-  const toyValue = document.querySelectorAll(".input-text")[0].value
-  const imageValue = document.querySelectorAll(".input-text")[1].value 
-
-let newToy = {
-    name: toyValue,
-    image: imageValue,
+  let newToy = {
+    name: toyInput,
+    image: imageInput,
     likes: 0
   }
-
   fetch(url, {
     method: "POST",
-    headers:
+    headers: 
   {
-    "Content-Type": "application/json",
+    "Content-Type" : `application/json`,
     Accept: "application/json",
   },
-  body: JSON.stringify(newToy),
-})
+    body: JSON.stringify(newToy),
+  })
   .then((response) => response.json())
-  .then((toy)  => renderOneCard(toy))
+  .then((toyData) => renderOneToy(toyData)) // returns what was added // function
 })
 
-// grab like button
-// 
-const allButtons = document.querySelectorAll(".like-btn")
-
-// // allButtons.forEach((button) => {
-// //   button.addEventListener("click", () => {
-// //     console.log(click)
-// //     // toy.likes++
-// //     // p.textContent = `${toy.likes} likes`
-//   })
-// })
+function handleDelete(id) {
+  fetch(`http://localhost:3000/toys/${id}` , {
+    method: "DELETE",
+    headers: {
+      "Content-Type" : `application/json`,
+       Accept: "application/json",
+    },
+  })
+  .then((response) => response.json())
+  .then((delateData) => console.log(delateData))
+}
